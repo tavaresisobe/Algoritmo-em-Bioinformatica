0e29fcc0e8692f59594fd90f2ba3acf6336a0acf
Gustavo Tavares Isobe <91920423+tavaresisobe@users.noreply.github.com>
2023-03-15 22:19:09 -0300

    Create lista01.py

---
+++ lista01.py
@@ -0,0 +1,44 @@
+#1)
+
+#nome = input("Informe seu nome:")#
+#altura = float(input("Informe sua altura:"))
+#peso = float(input("Informe seu peso:"))
+#imc = float(peso/(altura*altura))
+#print (f"{nome} tem {peso} kilos e a altura de {altura} e portanto o IMC é de {imc}")
+
+#2)
+
+#metros = float(input("Digite o valor medido em metros: "))
+#cm = (metros * 100)
+#print (f"O valor informado em centimetros é: {cm}")
+
+#3)
+#dias = int(input("Digite o numero de dias: "))
+#horas = int(input("Digite quantidade de horas: "))
+#minutos = int(input("Informe os minutos: "))
+#segundos = int(input("Digite a quantidade de segundos: "))
+#seg = int(dias * 24 * 3600 + horas * 3600 + minutos * 60 + segundos)
+#print (f"{seg} segundos")
+
+#4)
+
+#salario =  float(input("Informe o valor de seu salário: "))
+#porcent = float(input("Informe a porcentagem de aumento do seu salário: "))
+#aumento = float(salario+(porcent/100 * salario))
+#print (f"O salárrio após o reajuste de {porcent}% será de: R${aumento}")
+
+#5)
+
+#km = float(input("Informe a distancia percorrida em km: "))
+#dias = int(input("Digite o número de dias que o carro foi alugado: "))
+#preco = float((60*dias)+(0.15*km))
+#print (f"O preço será de: R${preco}")
+
+#6)
+
+#r1 = float(input("Informe o valor de R1: "))
+#r2 = float(input("Informe o valor de R2: "))
+#r3 = float(input("Informe o valor de R3: "))
+#r = 1 / ((1/r1) + (1/r2) + (1/r3))
+#print (f"A resistência resultante será de: {r}")
+
