ef9c63a1208683a5eddcee7b49fd5ea37a0fa14d
Gustavo Tavares Isobe <91920423+tavaresisobe@users.noreply.github.com>
2023-03-20 22:33:58 -0300

    Create lista02.py

---
+++ lista02.py
@@ -0,0 +1,40 @@
+1)
+#ph = float(input("Digite o pH da solução: "))
+#if ph < 7:
+#    print("A solução é ácida")
+#elif ph == 7:
+#    print("A solução é neutra")
+#elif ph > 7 :
+#    print("A solução é básica")
+
+#2)
+#s = input("Continuar (s/n)?\n")
+#while True:
+#    if s.lower() == 's':
+#        s = input("Continuar (s/n)?\n")
+#    elif s.lower() == 'n':
+#        print("OK, parando...\n")
+#        break
+#    else:
+#        print ("Erro...\n")
+#        s = input("Continuar (s/n)?\n")
+
+#3)
+#print ("Iniciando à contagem...")
+#for i in range(10,-1,-1):
+#    print (i)
+#print ("...fogo!")
+
+#4)
+s1 = '1'
+s2 = '1'
+s3 = '9'
+for i in range(2, 11, 1):
+#   for n in range (11, 2, -1):
+    print (f"{s1} x 8 + {s2} = {s3}")
+    s1 += str(i)
+    s2 = str(i)
+#    s3 += str(n)
+
+#5)
+
