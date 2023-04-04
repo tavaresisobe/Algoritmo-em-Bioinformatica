174d197d43fef76519de549751e62b70e1551d68
Gustavo Tavares Isobe <91920423+tavaresisobe@users.noreply.github.com>
2023-04-03 21:22:42 -0300

    Update checarcpf.py

--- checarcpf.py
+++ checarcpf.py
@@ -18,3 +18,29 @@ s_2 = sum(aux_2)
 print(s_2)
 check_2 = s * 10 / 11
 print(check_2)
+
+
+################################################
+import random
+cpf = [random.randint(0,10) for i in range (9)]
+p1 = list(range(11:1:-1))
+
+d1 = [i * j for i, j in zip(l,p1)]
+d1 = sum(d1)%11
+
+if d1 < 2:
+    d1 = 0
+else
+    d1 = 11 - d1
+cpf.append(d1)
+
+p2 = list(range(11:1:-1))
+
+d2 = [i * j for i, j in zip(l,p2)]
+d2 = sum(d2)%11
+
+if d2 < 2:
+    d2 = 0
+else
+    d2 = 11 - d2
+cpf.append(d2)
